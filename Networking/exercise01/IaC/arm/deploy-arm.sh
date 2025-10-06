04faa5c85f3844d395c289da5c447b35ea7b0235
gtpdev-net <149839821+gtpdev-net@users.noreply.github.com>
2025-10-06 12:28:57 +0000

    Adds pure ARM Template implementation

---
+++ Networking/exercise01/IaC/arm/deploy-arm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Deploy ARM template for networking resources
+
+RESOURCE_GROUP="RG1"
+LOCATION="eastus"
+TEMPLATE_FILE="$(dirname "$0")/main.json"
+
+az group create --name "$RESOURCE_GROUP" --location "$LOCATION"
+az deployment group create \
+  --resource-group "$RESOURCE_GROUP" \
+  --template-file "$TEMPLATE_FILE" \
+  --parameters location="$LOCATION"
